3802a9dd9041070f69ad00772efb271b2b5c0be9
KHALMATOV-FARRUKH <khalmatov.farrukh@gmail.com>
2023-03-12 15:54:10 +0500

    Created: categories_widget.dart
    Signed-off-by: KHALMATOV-FARRUKH <khalmatov.farrukh@gmail.com>

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:badges/badges.dart' as badges;
+import 'package:grocery_store_app_ui/widgets/categories_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -35,13 +37,74 @@ class HomePage extends StatelessWidget {
                     ),
                     child: InkWell(
                       onTap: () {},
-                      child: Icon(
-                        CupertinoIcons.cart,
-                        size: 30,
-                        color: Colors.white,
+                      child: badges.Badge(
+                        badgeContent: Text('3'),
+                        child: Icon(
+                          CupertinoIcons.cart,
+                          size: 25,
+                          color: Colors.white,
+                        ),
                       ),
                     ),
                   ),
+                ],
+              ),
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Welcome",
+                    style: TextStyle(fontSize: 35, color: Colors.white),
+                  ),
+                  Text(
+                    "What do you want to Buy?",
+                    style: TextStyle(fontSize: 20, color: Colors.white),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Row(
+                children: [
+                  Icon(Icons.search),
+                  Container(
+                    margin: EdgeInsets.only(left: 10),
+                    width: 250,
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        hintText: "Search",
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                  Spacer(),
+                  Icon(Icons.filter_list),
+                ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(top: 20),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30),
+                  topRight: Radius.circular(30),
+                ),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  CategoriesWidget(),
                 ],
               ),
             ),
